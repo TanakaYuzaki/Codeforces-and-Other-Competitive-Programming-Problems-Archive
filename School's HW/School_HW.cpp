398484f1ef714bde4375832b20b74b0e34d7f345
Duy Khang <131791276+TanakaYuzaki@users.noreply.github.com>
2025-11-06 09:30:00 +0700

    Add files via upload
    Problem 5
     Write a program that inputs a month and a year. The program then performs the following requirements:
     • Check if the given year is a leap year. A leap year is divisible by 4 but not by 100, or is divisible by 400.
     1
    • Output the number of days in the given month. Months with 31 days: 1, 3, 5, 7, 8, 10, 12; months with 30
     days: 4, 6, 9, 11; February has 28 or 29 days

---
+++ School's HW/School_HW.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void check(int year,int days){
+    if((year%4 == 0 && !(year%100 == 0)) || (year%4 == 0 && year%400 == 0)){
+        cout<<"This is a leap year";
+        if(days == 29){
+            cout<<2;
+        }
+    }
+
+    if(days == 31){
+        cout<<1<<" "<<" "<<3<<" "<<5<<" "<<7<<" "<<8<<" "<<10<<" "<<12;
+    }
+    if(days == 30){
+        cout<<2<<" "<<4<<" "<<6<<" "<<9<<" "<<11;
+    }
+    if(days == 28){
+        cout<<2;
+    }
+
+
+    
+    
+
+
+}
+int main(){
+    int year,days;
+    cin>>year>>days;
+    check(year,days);
+}
